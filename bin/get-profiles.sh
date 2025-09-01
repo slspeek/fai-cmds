3838bf3c7a6755776c92c094e21f59c3756f2e0f
Steven L. Speek <slspeek@gmail.com>
2025-09-01 13:54:00 +0200

    extra newline between profiles

--- bin/get-profiles.sh
+++ bin/get-profiles.sh
@@ -3,7 +3,11 @@
 set -e
 FAI_CONFIG=$1
 
-cat $FAI_CONFIG/class/*.profile | \
+for PROFILE in $FAI_CONFIG/class/*.profile;
+do 
+  cat $PROFILE
+  echo
+done| \
   awk '/^Name: / {for(i=2; i<=NF; i++){printf("%s", tolower($i));\
      if(i!=NF){printf("-")}}} \
      /^Classes: / \
